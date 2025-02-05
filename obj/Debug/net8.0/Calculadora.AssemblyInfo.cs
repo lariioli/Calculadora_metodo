1f1a440540d52c7dd6d4bd82108e81988f7adf34
Larissa de Oliveira <larissaoliveira3zb@gmail.com>
2025-02-05 14:30:13 -0300

    Calculadora: adicionando o método de adição

--- obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
+++ obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cc0a6ab0bca7c6d80a9695bb96e6ea1cfbf9fd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daccd8b4d6685acb9ce40d08def39f050b22b05b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
