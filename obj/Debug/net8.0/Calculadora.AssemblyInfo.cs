de560d0a45f8bc6478659cce3321253635c2fb8f
larissa de Oliveira <larissaoliveira3zb@gmail.com>
2025-02-10 13:40:44 -0300

    Finalizando calculadora

--- obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
+++ obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+397396bb6584e5d941954d88f008a11256cfb95b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58ebdd3810c9f327a6672f24f818fd03eebf75a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
